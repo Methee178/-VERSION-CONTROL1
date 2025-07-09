3f0971e6fac1214bcebba5069d15f1aa79c334a8
methee178 <devilpop@gmail.com>
2025-07-09 10:58:00 +0700

    edit label with thai language

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,10 +9,10 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ นามสกุล *"
+        hint="กรุณาใส่ชื่อชื่อนามสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'โปรดใส่ข้อความที่ถุกต้อง']"
       />
 
       <q-input
